37c5d3299c63619226dfaa8b155afa801edcf4fb
Clement Juste <clement.juste@zenika.com>
2014-04-10 23:27:36 +0200

    liste des liens vers une page

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,15 +1,14 @@
 var Controllers = angular.module('Controllers', ['Services']);
 
 Controllers.controller('HomeController', function ($scope, $location, $http) {
-	$scope.result = new Object ();
-	$scope.result.url = "";
+	$scope.results = [];
     $scope.goHome = function () {
         $location.path("/");
     };
 
     $scope.searchText = function () {
     	$http({method:'GET', url:'/api/search', params:{'text' : $scope.search}, headers:{'Accept':'application/json'}}).success(function (data, status, headers, config) {
-    		$scope.result = data;
+    		$scope.results = data;
     	});
     }
 });
